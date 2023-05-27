df92c32acc1044d0fff86a155002d5565719f6c1
Ning Guo <42087775+Gabriel-Ning@users.noreply.github.com>
2023-05-27 11:01:52 +0800

    Update __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,4 @@
 from .VisualForceRegression import VisualFingerForceNet
 from .VisualFingerVAE import VisualFingerVAE
 from .VisualForceVAE import VisualForceVAE
-
-
+from .VisualForceRegression_v2 import VisualFingerForceNet_v2
